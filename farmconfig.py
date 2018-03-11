e736578f77bec2d4c43f985b51cc810020ef5d64
Andrey Kurenkov <andreyvkurenkov@gmail.com>
2018-03-11 16:50:09 +0000

    Correct farmconfig

--- farmconfig.py
+++ farmconfig.py
@@ -1,31 +1,13 @@
-# -*- coding: utf-8 -*-
+# -*- coding: iso-8859-1 -*-
 
-from farmconfig import FarmConfig
+wikis = [
+    ("aiwiki",  r"^.*$"),
+]
 
-class Config(FarmConfig):
-    sitename = u'The AI Wiki'
-    interwikiname = 'aiwiki'
-    show_interwiki = False
-    logo_string = u'<img src="/moin_static198/common/aiwiki.png" alt="AI Wiki Logo"><h4 id="The_AI_Wiki">The AI Wiki</h4>'
-    acl_rights_default = u'Known:read,write,delete,revert All:read,write  +AdminGroup:admin BadGuy:'
+from MoinMoin.config.multiconfig import DefaultConfig
 
-    page_front_page = u"Overview" 
+class FarmConfig(DefaultConfig):
+
+    data_underlay_dir = '/test/wikifarm/underlay/'
     
-    data_dir = '/var/www/moin/aiwiki/data/'
-    data_underlay_dir = '/var/www/moin/aiwiki/underlay/'
-    cache_dir = '/var/lib/moin/cache'
-    user_checkbox_defaults = {'show_page_trail':False}
 
-    superuser = ['admin','AndreyK']
-    theme_default = u"hal"
-    trail_size = 0
-
-    mail_from = u"MoinMoin notifier <admin@example.com>"
-    mail_smarthost = "127.0.0.1"
-
-    editor_force = False
-    editor_default = 'gui'
-    editor_ui = 'freechoice'
-
-    secrets = '72451f66a0e31a454c01d6c3cedca46a6946b567796d333073906a792549e2a2'
-    secrets = '00c56057b27790305f4bf76bd0e0180e670e3a817b97c0e95b92d727e79206f5'
